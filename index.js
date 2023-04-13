8d6880db7c9d40144b334f028ee0828252fc1ab4
temple-19 <tozoaz@gmail.com>
2023-04-13 14:01:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,10 @@ app.use('/api/users', usersRoute);
 app.use('/api/hotels', hotelsRoute);
 app.use('/api/rooms', roomsRoute);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || 'Something went wrong';
