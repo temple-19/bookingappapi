26eb8cfd9ebaf858bea6748f9e32cfc42f07aa7a
temple-19 <tozoaz@gmail.com>
2023-04-14 23:11:42 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import path from 'path';
 import { login, register } from '../controllers/auth.js';
 
 const router = express.Router();
 
+router.use(express.static(path.join(__dirname, 'build')));
 
+// Serve the index.html file for all other routes
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 router.post('/register', register);
 router.post('/login', login);
-router.get('/login', (req, res) => {
-  res.send('Welcome to the users page!');
+router.get('/login',(req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
