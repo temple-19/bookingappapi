84d96008d32fbd500fd98e42286cd0f4a44ea9b1
temple-19 <tozoaz@gmail.com>
2023-04-14 12:39:36 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ import { login, register } from '../controllers/auth.js';
 const router = express.Router();
 
 router.post('/register', register);
-router.post('/login', login);
+router.post('https://chipper-pie-6f9780.netlify.app/login', login);
 
 export default router;
