8839a8b551165be80b887c5dc801fa4e20024e98
temple-19 <tozoaz@gmail.com>
2023-04-14 22:25:15 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ import { login, register } from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get('/login', console.log('mister'))
+
 router.post('/register', register);
 router.post('/login', login);
 
