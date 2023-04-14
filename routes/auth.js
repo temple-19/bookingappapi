19aa145b7b410a7f78f325b83b1e56bbebeb0f94
temple-19 <tozoaz@gmail.com>
2023-04-14 22:51:46 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/login', (req, res) => {
-  res.send('Hello, World!');
+  console.log("There is hope")
 })
 
 
