fbd16cc227ac5d4009333fa76e15bce84d8a30fb
temple-19 <tozoaz@gmail.com>
2023-04-14 10:48:34 +0100

    url change

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ import { login, register } from '../controllers/auth.js';
 
 const router = express.Router();
 
+router.post('https://chipper-pie-6f9780.netlify.app/register', register);
+router.post('https://chipper-pie-6f9780.netlify.app/login', login);
 router.post('/register', register);
 router.post('/login', login);
 
